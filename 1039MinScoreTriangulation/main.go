8b999ccd0b97b3919cfb66c92b037440ebf2cd4d
易剑东 <yijiandong@primedu.cn>
2019-12-23 19:54:03 +0800

    add code 1039

--- 1039MinScoreTriangulation/main.go
+++ 1039MinScoreTriangulation/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println(minScoreTriangulation([]int{1,2,3}) == 6)
+    fmt.Println(minScoreTriangulation([]int{1,3,1,4,1,5}) == 13)
+}
 
 func minScoreTriangulation(A []int) int {
 
@@ -9,17 +17,39 @@ func minScoreTriangulation(A []int) int {
         return A[0] * A[1] * A[2]
     }
 
-    dp := make([][]int, n)
-    for i := 0; i < n ; i++ {
-        dp[i] = make([]int , n)
+    cache := make([]int, n*n+1)
+
+    return subOrder(cache, A, 0, n-1, n)
+
+}
+
+func subOrder(cache []int, A []int, a, b int, n int) int {
+
+    if b == a + 1 {
+        return 0
     }
 
-    for l := 2; l < n; l++ {
-        for i := 0; i < n; i++ {
+    t := (a)*n + b
 
-        }
+    if cache[t] != 0 {
+        return cache[t]
     }
 
+    m := 100000000
+    for i := a + 1; i < b; i++ {
+        m = min(m, A[a]*A[b]*A[i] + subOrder(cache, A, a, i, n) + subOrder(cache, A, i, b, n))
+    }
 
+    cache[t] = m
+
+    return m
+}
+
+func min(a, b int) int {
+    if a > b {
+        return b
+    }
+
+    return a
 }
 
